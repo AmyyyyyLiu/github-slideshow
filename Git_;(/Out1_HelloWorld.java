5d67b12a7e4014480f31d3c5227e424ef47effbb
AmyyyyyLiu <81171292+AmyyyyyLiu@users.noreply.github.com>
2021-03-23 10:07:40 -0400

    Add files via upload

---
+++ Git_;(/Out1_HelloWorld.java
@@ -0,0 +1,13 @@
+/* 
+ * Date: Feb 16, 2021
+ * Name: Amy Xiaoshan Liu
+ * Teacher: Mr.Ho
+ * Description: my first hello world program
+ * */
+
+
+class Out1_HelloWorld{
+  public static void main(String args[]) {   
+    System.out.println("Hello World!!"); // output a message
+ }
+}
